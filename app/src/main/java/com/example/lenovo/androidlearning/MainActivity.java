a81c5276b93837d2fc2df49aa8e26a222d4aa5b7
inspurer <2391527690@qq.com>
2018-12-22 18:56:03 +0800

    add MainActivity

--- app/src/main/java/com/example/lenovo/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/lenovo/androidlearning/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.print("hello_world");
         setContentView(R.layout.activity_main);
     }
 }
