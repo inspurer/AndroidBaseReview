85ccf6ab309f6d3b163762baa12b7c195417e857
inspurer <2391527690@qq.com>
2018-12-23 17:31:53 +0800

    update TextView

--- app/src/main/java/com/example/lenovo/androidlearning/TextViewActivity.java
+++ app/src/main/java/com/example/lenovo/androidlearning/TextViewActivity.java
@@ -1,14 +1,18 @@
 package com.example.lenovo.androidlearning;
 
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
 import android.widget.TextView;
 
 public class TextViewActivity extends AppCompatActivity {
 
-    private TextView mtv4,mtv5,mtv6;
+    private TextView mtv4,mtv5,mtv6,mtv8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +25,21 @@ public class TextViewActivity extends AppCompatActivity {
         mtv5 = findViewById(R.id.tv_5);
         mtv5.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //设置下划线
 
+        mtv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mtv6.animate().translationXBy(200).setDuration(1000);
+            }
+        });
+
         mtv6 = findViewById(R.id.tv_6);
         mtv6.setText(Html.fromHtml("<u>月小水长</u>"));
+
+        mtv8 = findViewById(R.id.tv_8);
+        // 给部分文字设置成色(2,4,也就是水长)
+        String text = "月小水长";
+        SpannableString spannableString= new SpannableString(text);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED),2,4, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mtv8.setText(spannableString);
     }
 }
