f6c4a25bd7ad6ffae3f53847d9f4b6f54afcb26c
inspurer <2391527690@qq.com>
2018-12-23 14:20:36 +0800

    add TextView

--- app/src/main/java/com/example/lenovo/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/lenovo/androidlearning/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.lenovo.androidlearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.print("hello_world");
         setContentView(R.layout.activity_main);
+        mtnTextView = findViewById(R.id.btn_tv);
+        mtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 跳转到TextView显示界面
+                Intent intent = new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
